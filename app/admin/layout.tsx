1fd150761efb7d7eef75dc37eab215dd4d94f91e
marynafedotova <mfedotova151@gmail.com>
2026-04-23 15:18:48 +0300

    fix admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,9 @@
 import { AdminShell } from "@/components/admin-ui";
 import type { ReactNode } from "react";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function AdminLayout({
   children
 }: Readonly<{
